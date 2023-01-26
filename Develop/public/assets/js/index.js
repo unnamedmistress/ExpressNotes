6497cc4959f0be7f5e8733a3beb9ede4e8814203
UnnamedMistress <momchrysti@gmail.com>
2023-01-26 18:32:01 -0500

    moved some files around

--- Develop/public/assets/js/index.js
+++ Develop/public/assets/js/index.js
@@ -4,13 +4,13 @@ let saveNoteBtn;
 let newNoteBtn;
 let noteList;
 
-if (window.location.pathname === '/notes') {
-  noteTitle = document.querySelector('.note-title');
-  noteText = document.querySelector('.note-textarea');
-  saveNoteBtn = document.querySelector('.save-note');
-  newNoteBtn = document.querySelector('.new-note');
-  noteList = document.querySelectorAll('.list-container .list-group');
-}
+// if (window.location.pathname === '/notes') {
+//   noteTitle = document.querySelector('.note-title');
+//   noteText = document.querySelector('.note-textarea');
+//   saveNoteBtn = document.querySelector('.save-note');
+//   newNoteBtn = document.querySelector('.new-note');
+//   noteList = document.querySelectorAll('.list-container .list-group');
+// }
 
 // Show an element
 const show = (elem) => {
@@ -22,7 +22,7 @@ const hide = (elem) => {
   elem.style.display = 'none';
 };
 
-// activeNote is used to keep track of the note in the textarea
+// activeNote  is used to keep track of the note in the textarea
 let activeNote = {};
 
 const getNotes = () =>
@@ -173,11 +173,11 @@ const renderNoteList = async (notes) => {
 // Gets notes from the db and renders them to the sidebar
 const getAndRenderNotes = () => getNotes().then(renderNoteList);
 
-if (window.location.pathname === '/notes') {
-  saveNoteBtn.addEventListener('click', handleNoteSave);
-  newNoteBtn.addEventListener('click', handleNewNoteView);
-  noteTitle.addEventListener('keyup', handleRenderSaveBtn);
-  noteText.addEventListener('keyup', handleRenderSaveBtn);
-}
+// if (window.location.pathname === '/notes') {
+//   saveNoteBtn.addEventListener('click', handleNoteSave);
+//   newNoteBtn.addEventListener('click', handleNewNoteView);
+//   noteTitle.addEventListener('keyup', handleRenderSaveBtn);
+//   noteText.addEventListener('keyup', handleRenderSaveBtn);
+// }
 
 getAndRenderNotes();
